101b7043f48241e346c2de04f9dc75a6b0eab28c
Nick <421075460@qq.com>
2023-09-23 21:19:15 +0930

    first commit

---
+++ public/javascripts/frontpage.js
@@ -0,0 +1,61 @@
+// TODO:
+// Impliment degree courses, pre_requisites, incompatibles
+// free to use the arrays in vue for form input
+
+const vuectrl = Vue.createApp({
+    data() {
+        return {
+            placeholder: "Result placeholder",
+            stream: [],
+            level: [],
+            course: [],
+            degree: [],
+            degree_course: [],
+            type: ["core", "elective","core & elective", "project", "core & project", "elective & project", "core, elective & project"],
+            term: ["Semester 1", "Semester 2", "All year"],
+            pre_requisites: [],
+            incompatibles: [],
+            errorMessage: null,
+            displayDegree: true
+        };
+    },
+    methods: {
+        
+        doSearching() {
+            console.log("Searching...");
+            },
+        doLogin() {
+            console.log("login...");
+            window.location.href = "login.html";
+        },
+        dataUpdate(){
+            vuectrl.fetchData("/api/course_streams", "stream");
+            vuectrl.fetchData("/api/degree_levels", "level");
+            vuectrl.fetchData("/api/courses", "course");
+            vuectrl.fetchData("/api/degrees", "degree");
+            // vuectrl.fetchData("/api/degree_courses", "degree_course");
+            // vuectrl.fetchData("/api/pre_requisites", "pre_requisite");
+            // vuectrl.fetchData("/api/incompatibles", "incompatibles");
+        },
+        fetchData(target_loc, dest_var){
+            let req = new XMLHttpRequest();
+            req.onreadystatechange = function () {
+                if (req.readyState === 4 && req.status === 200) {
+                    if (req.response) {
+                        
+                        vuectrl[dest_var] = JSON.parse(req.response);
+                    }
+                }else if (req.readyState === 4 && req.status !== 200){
+                    vuectrl.errorMessage = "Error fetching data from " + target_loc + ". Status: " + req.status;
+                }
+            };
+            req.open("GET", target_loc);
+            req.send();
+        }
+    }
+}).mount('#mainDiv');
+
+vuectrl.dataUpdate();
+
+
+
---
+++ routes/users.js
@@ -0,0 +1,99 @@
+var express = require('express');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;  
+var router = express.Router();
+const tokenManager = require('./tokenManager');
+
+
+router.post('/reg', (req, res) => {
+
+  const { email, password } = req.body;
+
+  
+  const query = "SELECT user_id FROM users WHERE email = ?";  
+
+  req.pool.query(query, [email], (error, results) => {
+      
+
+      if (error) {
+          
+          res.status(500).json({ status: 'ERROR', message: 'Internal Server Error' });
+          return;
+      }
+
+      if (results.length > 0) {
+        
+        res.json({ status: 'ERROR', message: "Email already registered." });
+    } else {
+        
+        const hashedPassword = bcrypt.hashSync(password, saltRounds);
+        console.log("reg, insert ", password, hashedPassword)
+        const insertUserQuery = "INSERT INTO users (email, password) VALUES (?, ?)";
+
+                
+        req.pool.query(insertUserQuery, [email, hashedPassword], (insertError, insertResults) => {
+            console.log("insert result----", insertError, insertResults);
+
+            if (insertError) {
+                res.status(500).json({ status: 'ERROR', message: 'Error while registering user.' });
+                return;
+            }
+            
+            
+            res.json({ status: 'OK', user_id: insertResults.insertId });
+        });
+    }
+  });
+});
+
+
+
+
+router.post('/login', (req, res) => {
+
+  const { email, password } = req.body;
+
+  
+  const query = "SELECT user_id,email,password FROM users WHERE email = ?";  
+
+  req.pool.query(query, [email], (error, results) => {
+      if (error) {
+          
+          res.status(500).json({ status: 'ERROR', message: 'Internal Server Error' });
+          return;
+      }
+
+      if (results.length > 0) {
+          console.log(results)
+
+          
+          const hashedPassword = bcrypt.hashSync(password, saltRounds);
+          console.log("login, fetch ", password, hashedPassword)
+        
+          fetch_password = results[0].password
+          fetch_user_id = results[0].user_id
+          fetch_email = results[0].email
+
+          const isMatch = bcrypt.compareSync(password, fetch_password);
+
+          if (isMatch) {           
+            console.log("found", fetch_email, fetch_password, fetch_user_id);
+
+            const token = tokenManager.generateToken();
+            console.log("new token generated", token);
+            tokenManager.storeToken(token, fetch_user_id);        
+            res.cookie('userToken', token, { maxAge: 900000, httpOnly: true });
+
+            res.json({ status: 'OK', user_id: fetch_user_id });
+          } else {
+            res.status(401).json({ status: 'ERROR', message: 'password incorrect' });
+          }
+      } else {
+          
+          console.log("not found");
+          res.status(401).json({ status: 'ERROR', message: 'User not found' });
+      }
+  });
+});
+
+module.exports = router;
